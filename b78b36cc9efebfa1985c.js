b68ebfc4cc0344a22f4929c60dc84a3e2f83c9da
CI Auto Build <tony@tonyhhyip.me>
2019-01-30 08:36:34 +0000

    CI: Build of 5ca59528374fb3b958f8d4eb4a304b681035d93a

--- b78b36cc9efebfa1985c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{174:function(e,t,n){"use strict";n.r(t);n(81);t.default={functional:!0,props:{event:{type:Object,required:!0}},render:function(e,t){var n=t.props,p=t.slots,r=t.children;return n.event.topic?e("nuxt-link",{props:{to:new URL(n.event.internal).pathname.replace("/event/","/topic/")},staticClass:"topicWrapper"},r):p().default[0]}}}}]);
-//# sourceMappingURL=b78b36cc9efebfa1985c.js.map
